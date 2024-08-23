62673f6680a9f38269b136f03f20383c87ce88ea
MAC <mac@MACs-MacBook-Air.local>
2024-08-23 17:45:17 +0100

    update new change

--- src/Components/page/RequestDemo.jsx
+++ src/Components/page/RequestDemo.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
 import Request from '../Request'
+import CoverDemo from '../CoverDemo'
 const RequestDemo= () => {
     return (
         <div className="">
             <Navbar />
+            <CoverDemo />
             <Request />
             <Footer />
         </div>
